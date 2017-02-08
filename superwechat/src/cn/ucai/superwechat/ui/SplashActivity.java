6496a6339d4b349d7fdb64d62e754940d05b7928
LCH <765353128@qq.com>
2017-02-08 10:52:50 +0800

    添加欢迎界面、登录注册、跳转以及添加ButterKnife

--- superwechat/src/cn/ucai/superwechat/ui/SplashActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/SplashActivity.java
@@ -2,13 +2,10 @@ package cn.ucai.superwechat.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.animation.AlphaAnimation;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.hyphenate.chat.EMClient;
 
-import cn.ucai.superwechat.DemoHelper;
+import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.R;
 
 import com.hyphenate.util.EasyUtils;
@@ -32,7 +29,7 @@ public class SplashActivity extends BaseActivity {
 
         new Thread(new Runnable() {
             public void run() {
-                if (DemoHelper.getInstance().isLoggedIn()) {
+                if (SuperWeChatHelper.getInstance().isLoggedIn()) {
                     // auto login mode, make sure all group and conversation is loaed before enter the main screen
                     long start = System.currentTimeMillis();
                     EMClient.getInstance().chatManager().loadAllConversations();
@@ -60,7 +57,7 @@ public class SplashActivity extends BaseActivity {
                         Thread.sleep(sleepTime);
                     } catch (InterruptedException e) {
                     }
-                    startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                    startActivity(new Intent(SplashActivity.this, GuideActivity.class));
                     finish();
                 }
             }
