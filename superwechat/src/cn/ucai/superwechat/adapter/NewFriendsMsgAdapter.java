eef7ec1565458d1d8ade1b36452451975808a1d7
LCH <765353128@qq.com>
2017-02-14 18:06:57 +0800

    新的朋友界面点击进入用户详情资料

--- superwechat/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
+++ superwechat/src/cn/ucai/superwechat/adapter/NewFriendsMsgAdapter.java
@@ -21,11 +21,15 @@ import com.hyphenate.easeui.utils.EaseUserUtils;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.db.InviteMessgeDao;
 import cn.ucai.superwechat.domain.InviteMessage;
+import cn.ucai.superwechat.ui.FriendProfileActivity;
+import cn.ucai.superwechat.ui.NewFriendsMsgActivity;
+import cn.ucai.superwechat.utils.MFGT;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -55,6 +59,7 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 			holder = new ViewHolder();
 			convertView = View.inflate(context, R.layout.em_row_invite_msg, null);
 			holder.avator = (ImageView) convertView.findViewById(R.id.avatar);
+			holder.layoutInvite = (LinearLayout) convertView.findViewById(R.id.layout_invite);
 			holder.reason = (TextView) convertView.findViewById(R.id.message);
 			holder.name = (TextView) convertView.findViewById(R.id.name);
 			holder.agree = (Button) convertView.findViewById(R.id.agree);
@@ -164,6 +169,12 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 				holder.status.setBackgroundDrawable(null);
 				holder.status.setEnabled(false);
 			}
+			holder.layoutInvite.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					MFGT.gotoFirent((NewFriendsMsgActivity)context,msg.getFrom());
+				}
+			});
 		}
 
 		return convertView;
@@ -282,6 +293,7 @@ public class NewFriendsMsgAdapter extends ArrayAdapter<InviteMessage> {
 	}
 
 	private static class ViewHolder {
+		LinearLayout layoutInvite;
 		ImageView avator;
 		TextView name;
 		TextView reason;
--- superwechat/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -16,7 +16,11 @@ import butterknife.OnClick;
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatHelper;
+import cn.ucai.superwechat.domain.Result;
+import cn.ucai.superwechat.net.NetDao;
+import cn.ucai.superwechat.net.OnCompleteListener;
 import cn.ucai.superwechat.utils.MFGT;
+import cn.ucai.superwechat.utils.ResultUtils;
 
 public class FriendProfileActivity extends BaseActivity {
 
@@ -51,12 +55,43 @@ public class FriendProfileActivity extends BaseActivity {
         imgBack.setVisibility(View.VISIBLE);
         txtTitle.setVisibility(View.VISIBLE);
         txtTitle.setText(R.string.userinfo_txt_profile);
-        user = (User) getIntent().getSerializableExtra(I.User.USER_NAME);
+        user = (User) getIntent().getSerializableExtra(I.User.TABLE_NAME);
         if (user != null) {
             showUserInfo();
         } else {
-            MFGT.finish(this);
+            String username = getIntent().getStringExtra(I.User.USER_NAME);
+            if (username == null) {
+                MFGT.finish(this);
+            } else {
+                syncUserInfo(username);
+            }
         }
+    }
+
+    private void syncUserInfo(String username) {
+        NetDao.getUserInfoByUserName(this, username, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s != null) {
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    if (result != null) {
+                        if (result.isRetMsg()) {
+                            User u = (User) result.getRetData();
+                            if (u != null) {
+                                user = u;
+                                showUserInfo();
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+
     }
 
     private void showUserInfo() {
